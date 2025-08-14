a956037848c510bde693483f995e0194641f8b7f
unknown <er.sukhdevkamboj@gmail.com>
2025-08-14 21:22:27 +0530

    import bootstrap package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 9 + Vue 2</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    
     <!-- Bootstrap JS (optional, for components like modals/dropdowns) -->
-
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <div id="app"></div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </html>
